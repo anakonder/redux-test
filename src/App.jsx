5f475ab3b432dc88fdff0763e5b63f57956ad999
anakonder <120507138+anakonder@users.noreply.github.com>
2023-08-12 02:48:48 +0300

    complite-counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector(state => state.cash)
-    console.log(cash)
+    
+    const addCash = (cash) => { 
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
     return(
         <div className={"app"}>
+            <div style={{fontSize: "3rem"}}>{cash}</div>
             <div style={{display: "flex"}}>
-                <button>Поповнити рахунок</button>
-                <button>Зняти з рахунку</button>
+                <button onClick={() => addCash(Number(prompt()))}>Поповнити рахунок</button>
+                <button onClick={() => getCash(Number(prompt()))}>Зняти з рахунку</button>
             </div>
         </div>
     )
